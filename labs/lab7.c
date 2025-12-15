942c9f78c9203b5b04cc9879b6306ff28d107555
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-12-15 08:03:21 +0300

    [change] update lab 7

--- labs/lab7.c
+++ labs/lab7.c
@@ -1,237 +1,227 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
-    char val;
-    struct Node *prev;
-    struct Node *next;
-} Node;
+#define DType char
 
-typedef struct {
-    Node *head;
-    Node *tail;
-    int size;
+typedef struct a {
+    DType data;
+    struct a *pNext;
+    struct a *pPrev;
 } List;
 
-static void list_clear(List *l) {
-    Node *cur = l->head;
-    while (cur) {
-        Node *nxt = cur->next;
-        free(cur);
-        cur = nxt;
-    }
-    l->head = l->tail = NULL;
-    l->size = 0;
+List *Init(void) {
+    return NULL;
 }
 
-static Node* list_at(List *l, int pos) {
-    if (pos < 0 || pos >= l->size) return NULL;
+int isEmpty(List *l) {
+    return (l == NULL);
+}
 
-    if (pos <= l->size / 2) {
-        Node *cur = l->head;
-        for (int i = 0; i < pos; ++i) cur = cur->next;
-        return cur;
-    } else {
-        Node *cur = l->tail;
-        for (int i = l->size - 1; i > pos; --i) cur = cur->prev;
-        return cur;
+int Count(List *l) {
+    int cnt = 0;
+    while (l != NULL) {
+        cnt++;
+        l = l->pNext;
+    }
+    return cnt;
+}
+
+void FreeList(List *l) {
+    while (l != NULL) {
+        List *nxt = l->pNext;
+        free(l);
+        l = nxt;
     }
 }
 
-static int list_insert(List *l, int pos, char v) {
-    if (pos < 0 || pos > l->size) return 0;
-
-    Node *x = (Node*)malloc(sizeof(Node));
-    if (!x) return 0;
-
-    x->val = v;
-    x->prev = x->next = NULL;
-
-    if (l->size == 0) {
-        l->head = l->tail = x;
-        l->size = 1;
-        return 1;
+List *Insert(List *l, int ind, DType new_data) {
+    int n = Count(l);
+    if (ind < 0 || ind > n) {
+        printf("Error: Illegal index\n");
+        return l;
     }
 
-    if (pos == 0) {
-        x->next = l->head;
-        l->head->prev = x;
-        l->head = x;
-        l->size++;
-        return 1;
+    List *newPtr = (List *)malloc(sizeof(List));
+    if (newPtr == NULL) {
+        printf("Error: memory error\n");
+        return l;
     }
 
-    if (pos == l->size) {
-        x->prev = l->tail;
-        l->tail->next = x;
-        l->tail = x;
-        l->size++;
-        return 1;
+    newPtr->data = new_data;
+    newPtr->pNext = NULL;
+    newPtr->pPrev = NULL;
+
+    if (ind == 0) {
+        newPtr->pNext = l;
+        if (l != NULL) l->pPrev = newPtr;
+        return newPtr;
     }
 
-    Node *at = list_at(l, pos);
-    if (!at) { free(x); return 0; }
+    List *p = l;
+    for (int i = 0; i < ind - 1; ++i) p = p->pNext;
 
-    x->prev = at->prev;
-    x->next = at;
-    at->prev->next = x;
-    at->prev = x;
-    l->size++;
+    newPtr->pNext = p->pNext;
+    newPtr->pPrev = p;
+
+    if (p->pNext != NULL) p->pNext->pPrev = newPtr;
+    p->pNext = newPtr;
+
+    return l;
+}
+
+int GetData(List *l, int ind, DType *out) {
+    int n = Count(l);
+    if (ind < 0 || ind >= n) {
+        printf("Error: Illegal index\n");
+        return 0;
+    }
+
+    for (int i = 0; i < ind; ++i) l = l->pNext;
+    *out = l->data;
     return 1;
 }
 
-static int list_delete(List *l, int pos) {
-    Node *x = list_at(l, pos);
-    if (!x) return 0;
+List *Delete(List *l, int ind) {
+    int n = Count(l);
+    if (ind < 0 || ind >= n) {
+        printf("Error: Illegal index\n");
+        return l;
+    }
 
-    if (x->prev) x->prev->next = x->next;
-    else l->head = x->next;
+    List *p = l;
+    for (int i = 0; i < ind; ++i) p = p->pNext;
 
-    if (x->next) x->next->prev = x->prev;
-    else l->tail = x->prev;
+    if (p->pPrev != NULL) p->pPrev->pNext = p->pNext;
+    else l = p->pNext;
 
-    free(x);
-    l->size--;
+    if (p->pNext != NULL) p->pNext->pPrev = p->pPrev;
 
-    if (l->size == 0) l->head = l->tail = NULL;
-    return 1;
+    free(p);
+    return l;
 }
 
-static int list_get(List *l, int pos, char *out) {
-    Node *x = list_at(l, pos);
-    if (!x) return 0;
-    *out = x->val;
-    return 1;
-}
-
-static void list_print_all(List *l) {
-    if (l->size == 0) {
+void PrintList(List *l) {
+    if (l == NULL) {
         printf("List is empty\n");
         return;
     }
 
-    Node *cur = l->head;
-    while (cur) {
-        printf("%c", cur->val);
-        if (cur->next) printf(" ");
-        cur = cur->next;
+    int cnt = 0;
+    while (l != NULL) {
+        printf("%d: %c\n", cnt, l->data);
+        cnt++;
+        l = l->pNext;
     }
+}
+
+static List *InsertNodeDesc(List *sorted, List *x) {
+    if (sorted == NULL) {
+        x->pPrev = x->pNext = NULL;
+        return x;
+    }
+
+    List *cur = sorted;
+    while (cur != NULL && cur->data >= x->data) cur = cur->pNext;
+
+    if (cur == sorted) {
+        x->pPrev = NULL;
+        x->pNext = sorted;
+        sorted->pPrev = x;
+        return x;
+    }
+
+    if (cur == NULL) {
+        List *tail = sorted;
+        while (tail->pNext != NULL) tail = tail->pNext;
+        tail->pNext = x;
+        x->pPrev = tail;
+        x->pNext = NULL;
+        return sorted;
+    }
+
+    x->pNext = cur;
+    x->pPrev = cur->pPrev;
+    cur->pPrev->pNext = x;
+    cur->pPrev = x;
+    return sorted;
+}
+
+List *SortDesc(List *l) {
+    List *sorted = NULL;
+
+    while (l != NULL) {
+        List *x = l;
+        l = l->pNext;
+
+        x->pPrev = x->pNext = NULL;
+        sorted = InsertNodeDesc(sorted, x);
+    }
+
+    return sorted;
+}
+
+void PrintMenu(void) {
     printf("\n");
-}
-
-static void insert_node_desc(Node **nh, Node **nt, Node *x) {
-    if (!*nh) {
-        *nh = *nt = x;
-        x->prev = x->next = NULL;
-        return;
-    }
-
-    Node *cur = *nh;
-    while (cur && cur->val >= x->val) cur = cur->next;
-
-    if (!cur) {
-        x->prev = *nt;
-        x->next = NULL;
-        (*nt)->next = x;
-        *nt = x;
-        return;
-    }
-
-    if (cur == *nh) {
-        x->prev = NULL;
-        x->next = *nh;
-        (*nh)->prev = x;
-        *nh = x;
-        return;
-    }
-
-    x->next = cur;
-    x->prev = cur->prev;
-    cur->prev->next = x;
-    cur->prev = x;
-}
-
-static void list_sort_desc(List *l) {
-    if (l->size < 2) return;
-
-    Node *nh = NULL, *nt = NULL;
-    Node *cur = l->head;
-
-    while (cur) {
-        Node *nxt = cur->next;
-        cur->prev = cur->next = NULL;
-        insert_node_desc(&nh, &nt, cur);
-        cur = nxt;
-    }
-
-    l->head = nh;
-    l->tail = nt;
-}
-
-static void print_menu(void) {
-    printf("\n");
-    printf("1 - init empty list\n");
-    printf("2 - insert by position (0..size)\n");
-    printf("3 - print element by position (0..size-1)\n");
-    printf("4 - delete by position (0..size-1)\n");
-    printf("6 - print all\n");
-    printf("7 - sort descending (B)\n");
-    printf("0 - exit\n");
+    printf("1) Init empty list\n");
+    printf("2) Insert (index, char)\n");
+    printf("3) Print element by index\n");
+    printf("4) Delete by index\n");
+    printf("5) Print all\n");
+    printf("6) Sort descending (B)\n");
+    printf("0) Exit\n");
     printf("\n");
 }
 
 int main(void) {
-    List l = { NULL, NULL, 0 };
+    List *l = Init();
     int cmd;
 
     while (1) {
-        print_menu();
+        PrintMenu();
         printf("cmd: ");
         if (scanf("%d", &cmd) != 1) break;
 
         if (cmd == 0) break;
 
         if (cmd == 1) {
-            list_clear(&l);
+            FreeList(l);
+            l = Init();
             printf("OK\n");
         }
         else if (cmd == 2) {
-            int pos;
-            char v;
-            printf("pos value: ");
-            if (scanf("%d %c", &pos, &v) != 2) {
+            int ind;
+            char ch;
+            printf("index char: ");
+            if (scanf("%d %c", &ind, &ch) != 2) {
                 printf("Error: bad input\n");
                 continue;
             }
-            if (!list_insert(&l, pos, v)) printf("Error: invalid position or memory\n");
-            else printf("OK\n");
+            l = Insert(l, ind, ch);
         }
         else if (cmd == 3) {
-            int pos;
-            char v;
-            printf("pos: ");
-            if (scanf("%d", &pos) != 1) {
+            int ind;
+            char ch;
+            printf("index: ");
+            if (scanf("%d", &ind) != 1) {
                 printf("Error: bad input\n");
                 continue;
             }
-            if (!list_get(&l, pos, &v)) printf("Error: no such position\n");
-            else printf("%c\n", v);
+            if (GetData(l, ind, &ch)) printf("%c\n", ch);
         }
         else if (cmd == 4) {
-            int pos;
-            printf("pos: ");
-            if (scanf("%d", &pos) != 1) {
+            int ind;
+            printf("index: ");
+            if (scanf("%d", &ind) != 1) {
                 printf("Error: bad input\n");
                 continue;
             }
-            if (!list_delete(&l, pos)) printf("Error: no such position\n");
-            else printf("OK\n");
+            l = Delete(l, ind);
+        }
+        else if (cmd == 5) {
+            PrintList(l);
         }
         else if (cmd == 6) {
-            list_print_all(&l);
-        }
-        else if (cmd == 7) {
-            list_sort_desc(&l);
+            l = SortDesc(l);
             printf("OK\n");
         }
         else {
@@ -239,6 +229,6 @@ int main(void) {
         }
     }
 
-    list_clear(&l);
+    FreeList(l);
     return 0;
 }
